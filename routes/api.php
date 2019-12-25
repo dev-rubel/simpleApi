c91cd4cc6eebddaa0ce3359a84f2ed89b9d8aa82
Md.Rubel <dpirubel@gmail.com>
2019-12-25 18:00:57 +0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::apiResource('/person', 'PersonController');
 
 Route::fallback(function () {
    return response()->json([
-      'message' => 'Page Not Found. If error persists, contact info@website.com'
+      'message' => 'Page Not Found'
    ], 404);
 });
