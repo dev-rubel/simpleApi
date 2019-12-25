4a4349d5534c6e626e918ada90624aa5d986338e
dpirubel <dpirubel@gmail.com>
2019-12-25 18:03:30 +0600

    change api.php

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::apiResource('/person', 'PersonController');
 
 Route::fallback(function () {
